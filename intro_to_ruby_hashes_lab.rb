873a9546ecd889aca974583174d8e355062213a6
Will Drougas <wdrougas@gmail.com>
2019-09-20 20:28:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
     },
     :names => {
-      :reading_railroad => {},
+      :reading_railroad => {:mortgage_value => "$100"},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline_railroad => {}
