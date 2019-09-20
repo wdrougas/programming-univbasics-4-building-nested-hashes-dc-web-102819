ae980b493f57d4769a772a55eb82835272aed08a
Will Drougas <wdrougas@gmail.com>
2019-09-20 20:24:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,8 +18,8 @@ def monopoly_with_third_tier
     :names => {
       :reading_railroad => {},
       :pennsylvania_railroad => {},
+      
       :shortline_railroad => {},
-      ::shortline_railroad
     }
    }
   }
